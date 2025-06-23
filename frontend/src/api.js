c36b31b38d1e50a79091e77e1381353c348a5c39
haha1250 <vidi@automationwr.de>
2025-06-23 16:35:15 +0200

    Initial commit

---
+++ frontend/src/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { ACCESS_TOKEN } from './constants';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,  // environment variable for API URL
+})
+
+// no need to set the base URL
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+export default api;
+// This code sets up an Axios instance with a base URL and an interceptor to add the access token to the headers of each request.
